e34e9b329b7873b9b755eb4c5fdfaba0d118180c
Prashant Kumar <prashant2003.hyd@gmail.com>
2025-07-29 19:23:27 +0530

    Add project files.

---
+++ MVCFormValidation/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFormValidation.Models;
+
+namespace MVCFormValidation.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public IActionResult Input()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                // If the model state is invalid, return the view with the current model
+                return View("Input", person);
+            }
+            return View(person);
+        }
+    }
+
+}
---
+++ MVCFormValidation/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations; //this one is for validation like using [Required], [StringLength(n)], etc.
+
+namespace MVCFormValidation.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="Name is Required")]
+        public string Name { get; set; }
+
+        [Range(1, 120, ErrorMessage = "Age must be between 1 and 120")]
+        public int Age { get; set; }
+    }
+}
